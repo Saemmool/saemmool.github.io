a979310dd2a468ea6626bd34d51cc047541da283
Saemmool <leesm0426@gmail.com>
2020-10-21 22:17:56 +0900

    exercise finished

--- data/chartjs/main.js
+++ data/chartjs/main.js
@@ -1,28 +1,54 @@
- var ctx = document.getElementById('chart').getContext('2d');
-            var myChart = new Chart(ctx, {
+const pollData = [
+    {
+     option: "Spider-Man", 
+     votes: 11,
+     color: "rgb(255, 99, 132)"
+    },
+
+    {
+     option: "Superman", 
+     votes: 8,
+     color: "rgb(54, 162, 235)"
+    },
+    {
+     option: "Batman", 
+     votes: 11,
+     color: "rgb(36, 36, 36)"
+    },
+    
+    {
+     option: "Son Goku", 
+     votes: 5,
+     color: "rgb(255, 159, 64)"
+    },
+    {
+     option: "Hulk", 
+     votes: 3,
+     color: "rgb(75, 192, 192)"
+    },
+    {
+     option: "Wolverine", 
+     votes: 8,
+     color: "rgb(255, 206, 8 6)"
+    },
+];
+
+function rgbToRgba(rgb, alpha=1) {
+    return `rgba(${rgb.substring(rgb.indexOf('(')+1, rgb.length-1).split(',').join()}, ${alpha})`;
+}
+
+Chart.defaults.global.defaultFontFamily = '"Comic Sans MS", cursive, sans-serif';
+
+const ctx = document.getElementById('chart').getContext('2d');
+ const myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: pollData.map(pollOption => pollOption.option),
             datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            data: pollData.map(pollOption => pollOption.votes),
+            backgroundColor: pollData.map(pollOption => rgbToRgba(pollOption.color, 0.75)),
+            borderWidth: 1,
             }]
         },
         options: {
@@ -32,6 +58,26 @@
                         beginAtZero: true
                 }
             }]
+        },
+        title: {
+            display: true,
+            text: 'Favorite Superheroes',
+            fontColor: "#333",
+            fontSize: 20,
+            padding: 20
+        },
+        legend: {
+            display: false
         }
     }
 });
+
+
+
+
+
+
+
+
+
+
