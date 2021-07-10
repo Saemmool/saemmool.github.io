56226193558932b653863893cacf13a6a62dfa50
Saemmool Lee <leesm0426@gmail.com>
2021-07-10 13:43:46 +0900

    [UPDATE] ADDED Design and programming
    Yeh!

--- personal.js
+++ personal.js
@@ -11,20 +11,20 @@ toggleBtn.addEventListener('click', () => {
 
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
-const container = document.querySelector('.designprogamming');
+const designprogramming = document.querySelector('.designprogramming');
 
 left.addEventListener('mouseenter', () => {
-    container.classList.add('hover-left');
+    designprogramming.classList.add('hover-left');
 });
 
 left.addEventListener('mouseleave', () => {
-    container.classList.remove('hover-left');
+    designprogramming.classList.remove('hover-left');
 });
 
 right.addEventListener('mouseenter', () => {
-    container.classList.add('hover-right');
+    designprogramming.classList.add('hover-right');
 });
 
 right.addEventListener('mouseleave', () => {
-    container.classList.remove('hover-right');
+    designprogramming.classList.remove('hover-right');
 });
