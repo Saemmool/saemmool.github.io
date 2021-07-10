c80b695af9a61e01fc7000d210639c03490c5df8
Saemmool Lee <leesm0426@gmail.com>
2021-07-10 10:36:38 +0900

    [UPDATE] removed tab

--- main.js
+++ main.js
@@ -7,28 +7,28 @@ toggleBtn.addEventListener('click', () => {
     icons.classList.toggle('active');
 });
 
-const tabItems = document.querySelectorAll('.tab-item');
-const tabContentItems = document.querySelectorAll('.tab-content-item');
+// const tabItems = document.querySelectorAll('.tab-item');
+// const tabContentItems = document.querySelectorAll('.tab-content-item');
 
-//select tab content item
-function selectItem(e) {
-    removeBorder();
-    removeShow();
-    //add border to current tab
-    this.classList.add('tab-border');
-    //Grab content item from DOM
-    const tabContentItem = document.querySelector(`#${this.id}-content`);
-    //add show class
-    tabContentItem.classList.add('show');
-}
+// //select tab content item
+// function selectItem(e) {
+//     removeBorder();
+//     removeShow();
+//     //add border to current tab
+//     this.classList.add('tab-border');
+//     //Grab content item from DOM
+//     const tabContentItem = document.querySelector(`#${this.id}-content`);
+//     //add show class
+//     tabContentItem.classList.add('show');
+// }
 
-function removeBorder() {
-    tabItems.forEach(item => item.classList.remove('tab-border'));
-}
+// function removeBorder() {
+//     tabItems.forEach(item => item.classList.remove('tab-border'));
+// }
 
-function removeShow() {
-    tabContentItems.forEach(item => item.classList.remove('show'));
-}
+// function removeShow() {
+//     tabContentItems.forEach(item => item.classList.remove('show'));
+// }
 
-// Listen for tab click
-tabItems.forEach(item => item.addEventListener('click', selectItem));
+// // Listen for tab click
+// tabItems.forEach(item => item.addEventListener('click', selectItem));
