cb42253d4087e8b85414be1f69df40f6bce960b4
Saemmool <leesm0426@gmail.com>
2021-07-21 09:02:20 +0900

    [Update]typo

--- personal13_folder/main.js
+++ personal13_folder/main.js
@@ -1,74 +1,77 @@
-const musicContainer = document.querySelector('.music-container');
-const playBtn = document.querySelector('#play');
-const prevBtn = document.querySelector('#prev');
-const nextBtn = document.querySelector('#next');
-const audio = document.querySelector('#audio');
-const progress = document.querySelector('.progress');
-const progressContainer = document.querySelector('.progress-container');
-const title = document.querySelector('#title');
-const cover = document.querySelector('cover');
+const musicContainer = document.getElementByID('music-container');
+const playBtn = document.getElementByID('play');
+const prevBtn = document.getElementByID('prev');
+const nextBtn = document.getElementByID('next');
+
+const audio = document.getElementByID('audio');
+const progress = document.getElementByID('progress');
+const progressContainer = document.getElementByID('progress-container');
+const title = document.getElementByID('title');
+const cover = document.getElementByID('cover');
+const currTime = document.querySelector('#currTime');
+const durTime = document.querySelector('#durTime');
 
 //Song titles
-const songs = ['hey', 'summer', 'uklele']
+const songs = ['hey', 'summer', 'mymusic'];
 
 //Keep track of the songs
-let songIndex = 2
+let songIndex = 2;
 
 //Initially load song into DOM
-loadSong(songs[songIndex])
+loadSong(songs[songIndex]);
 
 // Update song details 
 function loadSong(song) {
-    title.innerText = song
-    audio.src = `music/${mymusic}.mp3`
-    cover.src = `../images/${jeungbal}.png`
+    title.innerText = song;
+    audio.src = `music/${mymusic}.mp3`;
+    cover.src = `../images/${jeungbal}.png`;
 }
 
 function playSong() {
-    musicContainer.classList.add('play')
-    playBtn.querySelector('i.fas').classList.remove('fa-play')
-    playBtn.querySelector('i.fas').classList.add('fa-pause')
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
 
     audio.play()
 }
 
 function pauseSong() { 
-    musicContainer.classList.remove('play')
-    playBtn.querySelector('i.fas').classList.add('fa-play')
-    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
 
     audio.pause()
 }
 
 function prevSong() {
-    songIndex--
+    songIndex--;
 
     if(songIndex < 0) {
-        songIndex = songs.length - 1 
+        songIndex = songs.length - 1;
     }
 
-    loadSong(songs[songIndex])
+    loadSong(songs[songIndex]);
 
-    playSong()
+    playSong();
 }
 
 function nextSong() {
-    songIndex++
+    songIndex++;
 
     if(songIndex > songs.length - 1) {
-        songIndex = 0
+        songIndex = 0;
     }
 
-    loadSong(songs[songIndex])
+    loadSong(songs[songIndex]);
 
-    playSong()
+    playSong();
 
 }
 
 function updateProgress(e) {
-    const {duration, currentTime} = e.srcElement
-    const progressPercent = (currentTime / duration) * 100
-    progress.style.width = `${progressPercent}%`
+    const {duration, currentTime} = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
 }
 
 // Event listeners 
@@ -83,9 +86,9 @@ playBtn.addEventListener('click', () => {
 })
 
 function setProgress(e) {
-    const width = this.clientWidth
-    const clickX = e.offsetX
-    const duration = audio.duration
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
     
     audio.currentTime = (clickX / width) * duration
 }
