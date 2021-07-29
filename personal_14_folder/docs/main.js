9282b6a8d2c0a7bdb377ecd0fe8ff580324bfd50
Saemmool Lee <leesm0426@gmail.com>
2021-07-29 22:02:43 +0900

    [update] pdf

--- personal_14_folder/docs/main.js
+++ personal_14_folder/docs/main.js
@@ -14,7 +14,7 @@ const renderPage = num => {
     pageIsRendering = true;
 
 // Get page
-pdfDoc.getPage(num).then(page => {
+    pdfDoc.getPage(num).then(page => {
     // Set scale
     const viewport = page.getViewport({ scale });
     canvas.height = viewport.height;
@@ -22,13 +22,13 @@ pdfDoc.getPage(num).then(page => {
 
     const renderCtx = {
         canvasContext: ctx,
-        viewport
-    }
+        viewport 
+    };
 
     page.render(renderCtx).promise.then(() => {
         pageIsRendering = false;
 
-        if(pageNumIsPending !== null) {
+        if (pageNumIsPending !== null) {
             renderPage(pageNumIsPending);
             pageNumIsPending = null;
         }
@@ -39,11 +39,52 @@ pdfDoc.getPage(num).then(page => {
 });
 };
 
+// Check for pages rendering
+const queueRenderPage = num => {
+    if(pageIsRendering) {
+        pageNumIsPending = num;
+    } else {
+        renderPage(num);
+    }
+}
+
+// Show Prev Page 
+const showPrevPage = () => {
+    if(pageNum <= 1) {
+        return;
+    }
+    pageNum--;
+    queueRenderPage(pageNum);
+}
+
+// Show next Page 
+const showNextPage = () => {
+    if(pageNum >= pdfDoc.numPages) {
+        return;
+    }
+    pageNum++;
+    queueRenderPage(pageNum);
+}
+
+
 // Get Document
 pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
     pdfDoc = pdfDoc_;
 
     document.querySelector('#page-count').textContent = pdfDoc.numPages;
 
+    renderPage(pageNum);
+})
+    .catch(err => {
+        //display error
+        const div = document.createElement('div');
+        div.className = 'error';
+        div.appendChild(document.createTextNode(err.message));
+        document.querySelector('body').insertBefore(div, canvas);
+        //remove top bar
+        document.querySelector('.top-bar').style.display = 'none';
+    });
 
-});
+// Button events
+document.querySelector('#prev-page').addEventListener('click', showPrevPage);
+document.querySelector('#next-page').addEventListener('click', showNextPage);
