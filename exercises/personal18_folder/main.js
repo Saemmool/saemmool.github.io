7992db97b414e1a72879953f2668a594974eac6d
Saemmool Lee <leesm0426@gmail.com>
2021-08-11 17:35:28 +0900

    [ADD] Personal_18_responsive coming soon

---
+++ exercises/personal18_folder/main.js
@@ -0,0 +1,36 @@
+const countdown = document.querySelector('.countdown');
+
+// Set launch date in ms
+const launchDate = new Date('Jan 1, 2022 13:00:00').getTime();
+
+// Update every second
+const Intvl = setInterval(() => {
+    //get Today's date and time (ms)
+    const now = new Date().getTime();
+
+    //Distance from now to the launch date
+    const distance = launchDate - now;
+
+    //Calculation
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60* 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    //display results
+    countdown.innerHTML = `
+        <div>${days}<span>Days</span></div>
+        <div>${hours}<span>Hours</span></div>
+        <div>${mins}<span>Minutes</span></div>
+        <div>${seconds}<span>Seconds</span></div>
+        `;
+
+    // if launch date passed
+    if(distance < 0) {
+        //stop countdown
+        clearInterval(intvl);
+        //Style and output text
+        countdown.style.color = '#17a2b8';
+        countdown.innerHTML = 'Launched!';
+}
+}, 1000);
