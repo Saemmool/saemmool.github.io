0406836e5cce6ec9cbc046428b80d7efb895d5aa
Saemmool Lee <leesm0426@gmail.com>
2021-11-09 14:58:24 +0900

    [add] sandglass

--- mementoMori/drawing.js
+++ mementoMori/drawing.js
@@ -90,15 +90,6 @@ ctx2.arc(centerX2 + 40, centerY2 - 20, 10, 0, Math.PI * 2);
 ctx2.fillStyle = '#000000';
 ctx2.fill();
 
-
-        //Arc
-        // ctx.beginPath();
-        // const centerX = well.width / 2;
-        // const centerY = well.height / 2;
-        // ctx.arc(centerX, centerY, 200, 0, Math.PI * 2);
-        // ctx.strokeStyle = '#fff';
-        // ctx.stroke();
-
 //coal
 
 const coal = document.getElementById('coal');
@@ -161,5 +152,204 @@ ctx4.moveTo(220, 27);
 ctx4.lineTo(220, 244);
 ctx4.moveTo(280, 27);
 ctx4.lineTo(280, 244);
+ctx4.lineWidth = 5;
 ctx4.strokeStyle = '#999999';
 ctx4.stroke();
+
+//man
+const man = document.getElementById('man');
+const ctx5 = man.getContext('2d');
+
+man.width = 120;
+man.height = 240;
+
+ctx5.beginPath();
+const centerX5 = man.width / 2;
+const centerY5 = man.height / 2;
+//head
+ctx5.arc(centerX5, 30, 20, 10, 0, Math.PI * 2);
+//neck
+ctx5.moveTo(centerX5, 50);
+ctx5.lineTo(centerX5, 160);
+ctx5.moveTo(centerX5, 70);
+ctx5.lineTo(centerX5 - 45, 110);
+ctx5.moveTo(centerX5, 70);
+ctx5.lineTo(centerX5 + 45, 110);
+ctx5.moveTo(centerX5, 160);
+ctx5.lineTo(centerX5 - 45, 200);
+ctx5.moveTo(centerX5, 160);
+ctx5.lineTo(centerX5 + 45, 200);
+ctx5.lineWidth = 5;
+ctx5.strokeStyle = '#000';
+ctx5.stroke();
+
+//bible
+const bible = document.getElementById('bible');
+const ctx6 = bible.getContext('2d');
+
+bible.width = 200;
+bible.height = 240;
+
+//upper cover
+ctx6.beginPath();
+const centerX6 = bible.width / 2;
+const centerY6 = bible.height / 2;
+ctx6.moveTo(0, 30);
+ctx6.lineTo(170, 30);
+ctx6.lineTo(200, 0);
+ctx6.lineTo(30, 0);
+ctx6.lineTo(0, 30);
+ctx6.fillStyle = '#880000';
+ctx6.fill();
+ctx6.closePath();
+
+//main cover
+ctx6.beginPath();
+ctx6.moveTo(0, 30);
+ctx6.lineTo(0, 240);
+ctx6.lineTo(170, 240);
+ctx6.lineTo(170, 30);
+ctx6.lineTo(0,30);
+ctx6.fillStyle = '#000000';
+ctx6.fill();
+ctx6.closePath();
+
+//side cover
+ctx6.beginPath();
+ctx6.moveTo(170, 240);
+ctx6.lineTo(200, 210);
+ctx6.lineTo(200, 0);
+ctx6.lineTo(170, 30);
+ctx6.lineTo(170, 240);
+//fill text
+ctx6.font = '30px Arial';
+ctx6.fillStyle = 'white';
+ctx6.fillText('Holy Bible', 18, 100);
+ctx6.fillStyle = '#880000';
+ctx6.fill();
+ctx6.closePath();
+
+//life
+const life = document.getElementById('life');
+const ctx7 = life.getContext('2d');
+
+life.width = 450;
+life.height = 250;
+
+//line
+ctx7.beginPath();
+ctx7.moveTo(80, 200);
+ctx7.lineTo(380, 200);
+ctx7.moveTo(360, 180);
+ctx7.lineTo(380, 200);
+ctx7.lineTo(360, 220);
+ctx7.lineWidth = 5;
+ctx7.strokeStyle = '#000';
+ctx7.stroke();
+ctx7.closePath();
+//text
+ctx7.beginPath();
+ctx7.lineWidth = 1;
+ctx7.strokeStyle = 'black';
+ctx7.font = '20px serif';
+ctx7.fillStyle = '#000000';
+ctx7.fillText('Birth', 20, 205);
+ctx7.fillText('Death', 390, 206);
+ctx7.fillText('Life', 220, 230);
+ctx7.fill();
+ctx7.closePath();
+//arrow
+ctx7.beginPath();
+ctx7.moveTo(185, 180);
+ctx7.lineTo(200, 197);
+ctx7.lineTo(215, 180);
+ctx7.moveTo(200, 197);
+ctx7.quadraticCurveTo(200, 150, 300, 50);
+ctx7.lineWidth = 2;
+ctx7.strokeStyle = 'red';
+ctx7.stroke();
+ctx7.closePath();
+//another text
+ctx7.beginPath();
+ctx7.font = '20px Arial';
+ctx7.fillStyle = "#000000";
+ctx7.fillText('Where I was', 310, 50);
+
+//hourglass
+const sandglass = document.getElementById('sandglass');
+const ctx8 = sandglass.getContext('2d');
+
+sandglass.width = 150;
+sandglass.height = 200;
+
+//outer lines
+ctx8.beginPath();
+ctx8.moveTo(10, 10);
+ctx8.lineTo(140, 10);
+ctx8.quadraticCurveTo(125, 50, 90, 100);
+ctx8.quadraticCurveTo(125, 150, 140, 190);
+ctx8.lineTo(10, 190);
+ctx8.quadraticCurveTo(25, 150, 70, 100);
+ctx8.quadraticCurveTo(25, 50, 10, 8);
+ctx8.lineWidth = 5;
+ctx8.strokeStyle = '#000';
+ctx8.stroke();
+ctx8.closePath();
+
+//life text
+ctx8.beginPath();
+ctx8.font = '20px Arial';
+ctx8.fillStyle = "black";
+ctx8.fillText('LIFE', 55, 50);
+ctx8.fillText('DEATH', 43, 170);
+ctx8.closePath();
+
+//sands 
+ctx8.beginPath();
+const centerX8 = sandglass.width / 2;
+const centerY8 = sandglass.height / 2;
+ctx8.moveTo(centerX8 - 30, centerY8 - 40);
+ctx8.lineTo(centerX8 + 35, centerY8 - 40);
+ctx8.lineTo(centerX8 + 25, centerY8 - 30);
+ctx8.lineTo(centerX8 + 5, centerY8 - 5);
+ctx8.lineTo(centerX8 + 5, centerY8 - 5);
+ctx8.lineTo(centerX8 - 28, centerY8 - 40);
+ctx8.moveTo(centerX8 - 30, centerY8 + 50);
+ctx8.lineTo(centerX8 + 37, centerY8 + 50);
+ctx8.lineTo(centerX8 + 5, centerY8 + 5);
+ctx8.lineTo(centerX8 + 5, centerY8 + 5);
+ctx8.lineTo(centerX8 - 27, centerY8 + 40);
+ctx8.moveTo(centerX8 - 60, centerY8 + 90);
+ctx8.lineTo(centerX8 + 60, centerY8 + 90);
+ctx8.lineTo(centerX8 + 50, centerY8 + 75);
+ctx8.lineTo(centerX8 - 50, centerY8 + 75);
+ctx8.lineTo(centerX8 - 60, centerY8 + 85);
+ctx8.fillStyle = 'blue';
+ctx8.fill();
+ctx8.closePath();
+
+//line
+ctx8.beginPath();
+ctx8.moveTo(centerX8 + 4, centerY8 - 10);
+ctx8.lineTo(centerX8 + 4, centerY8 + 10);
+ctx8.moveTo(centerX8 - 20, centerY8 + 38);
+ctx8.lineTo(centerX8 - 60, centerY8 + 85);
+ctx8.moveTo(centerX8 + 24, centerY8 + 38);
+ctx8.lineTo(centerX8 + 61, centerY8 + 89);
+ctx8.lineWidth = 8;
+ctx8.strokeStyle = 'blue';
+ctx8.stroke();
+
+
+
+// ctx8.strokeStyle = 'blue';
+// ctx8.stroke();
+
+
+        //Arc
+        // ctx.beginPath();
+        // const centerX = well.width / 2;
+        // const centerY = well.height / 2;
+        // ctx.arc(centerX, centerY, 200, 0, Math.PI * 2);
+        // ctx.strokeStyle = '#fff';
+        // ctx.stroke();
