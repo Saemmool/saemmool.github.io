2d53d3949755c68d908f1f602aa811f190353ae7
Saemmool Lee <leesm0426@gmail.com>
2021-12-31 14:40:51 +0900

    [add] canvas test

--- canvasTest/galactic/script.js
+++ canvasTest/galactic/script.js
@@ -11,22 +11,24 @@ let mouse = {
     y: innerHeight / 2
 };
 
+const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 
-addEventListener('resize', function(){
+let mouseDown = false
+
+addEventListener('mousedown', () => {
+    mouseDown = true
+});
+
+addEventListener('mouseup', () => {
+    mouseDown = false
+})
+
+addEventListener('resize', () => {
     canvas.width = innerWidth;
     canvas.height = innerHeight;
 
     init();
 });
-
-//utility functions
-function randomIntFromRange(min,max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function randomColor(colors){
-    return colors(Math.floor(Math.random() * colors.length));
-}
 
 // Objects
 class Particle {
@@ -40,6 +42,8 @@ class Particle {
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.shadowColor = this.color
+        ctx.shadowBlur = 15
         ctx.fillStyle = this.color;
         ctx.fill();
         ctx.closePath();
@@ -51,15 +55,46 @@ class Particle {
 }
 
 // Implementation
+let particles
 function init() {
-
+    particles = []
+    for (let i = 0; i < 200; i++) {
+        const canvasWidth = canvas.width + 300
+        const canvasHeight = canvas.height + 300
+        const x = Math.random()* canvasWidth - canvasWidth / 2
+        const y = Math.random() * canvasHeight - canvasHeight / 2
+        const radius = 2 * Math.random()
+        const color = colors[Math.floor(Math.random() * colors.length)]
+        particles.push(new Particle(x,y,radius,color))
+        console.log(particles)
+    }
 }
 
 //Animation Loop
+let radians = 0
+let alpha = 1
 function animate() {
     requestAnimationFrame(animate);
+    ctx.fillStyle = `rgba(10, 10, 10, ${alpha})`
+    ctx.fillRect(0,0,canvas.width,canvas.height);
 
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    ctx.save()
+    ctx.translate(canvas.width/2, canvas.height/2)
+    ctx.rotate(0.01)
+    ctx.rotate(radians)
+    particles.forEach(particle => {
+        particle.update()
+    })
+    ctx.restore()
+    
+    radians += 0.001
+
+    if (mouseDown && alpha >= 0.1) {
+        alpha -= 0.01
+    } else if (!mouseDown && alpha < 1) {
+        alpha += 0.01
+    }
 }
 
 init();
